2a24b342666c4a8cc6d6eb1f7f2e32dbc487e3b4
Kaavya2828 <kaavyasinghal2828@gmail.com>
2017-02-27 12:15:51 -0800

    Stuff
    Hi

---
+++ Demo.java
@@ -0,0 +1,27 @@
+
+/**
+ * This will be our demo. Each student should add their own method to the code
+ */
+public class Demo
+{
+    // instance variables
+    private int x;
+
+    /**
+     * Constructor for objects of class Demo
+     */
+    public Demo()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * Getter for instance variable x.
+     *
+     */
+    public int getX()
+    {
+        return x;
+    }
+}
